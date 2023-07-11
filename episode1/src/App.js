46cf089783d0a793b68251654fb683abb3615d28
Tyson Nguyen <yngu4822@gmail.com>
2023-07-11 15:46:52 +1000

    :hammer: manipulate list and objects in jsx

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 function App() {
+  
   return(
     <div className='App'>
     </div>
@@ -8,33 +9,3 @@ function App() {
 }
 
 export default App;
-
-// 1. Learning Components and props
-// function App() {
-//   return (
-//   <div className='App'>
-//     <Job salary={125000} position="Full-stack Developer" company="Google" />
-//     <Job salary={140000} position="Software Developer" company="Microsoft" />
-//     <Job salary={140000} position="Application Developer" company="Apple" />
-//   </div>);
-// }
-
-// const Job = (props) => {
-//   return(
-//   <div>
-//     <h1>{props.position}</h1>
-//     <p>{props.salary}</p>
-//     <p>{props.company}</p>
-//   </div>)
-// };
-
-//2. Ternary Operators in react
-// function App() {
-//   const age = 17;
-//   const isGreen = true;
-//   return(
-//     <div className='App'>{age >= 18 ? <h2>Over age</h2> : <h2>Under age</h2>}
-//         <h1 style={{color: isGreen ? "green" : "red"}}>COLOR TEXT</h1>
-//     </div>
-//   );
-// }
---
+++ episode1/src/backlogs.js
@@ -0,0 +1,51 @@
+// 1. Learning Components and props
+// function App() {
+//   return (
+//   <div className='App'>
+//     <Job salary={125000} position="Full-stack Developer" company="Google" />
+//     <Job salary={140000} position="Software Developer" company="Microsoft" />
+//     <Job salary={140000} position="Application Developer" company="Apple" />
+//   </div>);
+// }
+
+// const Job = (props) => {
+//   return(
+//   <div>
+//     <h1>{props.position}</h1>
+//     <p>{props.salary}</p>
+//     <p>{props.company}</p>
+//   </div>)
+// };
+
+//2. Ternary Operators in react
+// function App() {
+//   const age = 17;
+//   const isGreen = true;
+//   return(
+//     <div className='App'>{age >= 18 ? <h2>Over age</h2> : <h2>Under age</h2>}
+//         <h1 style={{color: isGreen ? "green" : "red"}}>COLOR TEXT</h1>
+//     </div>
+//   );
+// }
+
+//3. Manipulating objects with list and components for clean code
+// function App() {
+//     const planets = [
+//       {name: "Mars", isGasPlanet: false},
+//       {name: "Earth", isGasPlanet: false},
+//       {name: "Jupiter", isGasPlanet: true},
+//       {name: "Venus", isGasPlanet: true},
+//       {name: "Neptune", isGasPlanet: true},
+//       {name: "Uranus", isGasPlanet: true},
+//     ];
+    
+//     return(
+//       <div className='App'>
+//         {planets.map((planet,key) => {
+//          return(         
+//           planet.isGasPlanet && <h1>{planet.name}</h1>)
+//           })}
+//       </div>
+//     );
+//   }
+
