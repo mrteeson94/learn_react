dd6d98379d0a7a4895755c249e7edfb4d3e0e082
Tyson Nguyen <yngu4822@gmail.com>
2023-07-11 14:09:43 +1000

    :hammer: updated exercise for components & props

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
 
 function App() {
-  const name = "Tyson";
-  const age = 28;
-  const email = "yngu4822@gmail.com";
-  const user = 
-  <div>
-    <p>{name}</p>
-    <p>{age}</p>
-    <p>{email}</p>
-  </div>;
-
-  return (
-  <div className='App'>
-    {user}
-  </div>);
+  return(
+    <div className='App'>Hello World</div>
+  );
 }
 
-
 export default App;
+
+// 1. Learning Components and props
+// function App() {
+//   return (
+//   <div className='App'>
+//     <Job salary={125000} position="Full-stack Developer" company="Google" />
+//     <Job salary={140000} position="Software Developer" company="Microsoft" />
+//     <Job salary={140000} position="Application Developer" company="Apple" />
+//   </div>);
+// }
+
+// const Job = (props) => {
+//   return(
+//   <div>
+//     <h1>{props.position}</h1>
+//     <p>{props.salary}</p>
+//     <p>{props.company}</p>
+//   </div>)
+// };
