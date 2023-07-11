66c93933c834832c8ef095a104722aeda03de126
Tyson Nguyen <AKATYSON_XD@hotmail.com>
2023-07-11 12:02:39 +1000

    :sparkles: components covered

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 function App() {
+  const name = "Tyson";
+  const age = 28;
+  const email = "yngu4822@gmail.com";
+  const user = 
+  <div>
+    <p>{name}</p>
+    <p>{age}</p>
+    <p>{email}</p>
+  </div>;
+
   return (
-    <div className="App">
-    </div>
-  );
+  <div className='App'>
+    {user}
+  </div>);
 }
+
 
 export default App;
