cc383410362cd89b8abccd4057b8eb16c648cf8f
Tyson Nguyen <yngu4822@gmail.com>
2023-07-13 11:13:04 +1000

    :sparkles: counter task using states

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  const [count,setCount] = useState(0);
+
+  const IncreaseCount = (event) => {
+    setCount(count + 1)
+  };
+
+  const DecreaseCount = (event) => {
+    setCount(count - 1)
+  };
+
+  const SetToZero = (event) => {
+    setCount(0)
+  };
+  
   return(
     <div className='App'>
+    <button onClick={IncreaseCount}>Increase</button>
+    <button onClick={DecreaseCount}>Decrease</button>
+    <button onClick={SetToZero}>SetZero</button>
+    {count}
+
     </div>
   );
 }
